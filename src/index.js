d80956b0a4a806359bf904d4d516819f3e4bc5a4
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-08-09 12:54:27 -0300

    footer

--- src/index.js
+++ src/index.js
@@ -2,14 +2,33 @@ import { fromPairs } from "lodash";
 import './style.css';
 import icon from './assets/icon.png';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
-  
+const retrieveFromAPI = async () => {
+  const res = await fetch(`${baseUrl}/games/KiAaECHQt9kyqpbrRb7E/scores`);
+  const lead = await res.json();
+  const leaders = lead.result;
+  return leaders;
+};
+const leaderBoardWrapper = document.getElementById('scoreList');
+const scoreBoard = ({ user, score }) => {
+  leaderBoardWrapper.innerHTML += `<li class="animated bounce"><span>${user}</span><span class="span">${score}</span></li>`;
+};
+
+const displayComments = async () => {
+  const comment = await retrieveFromAPI();
+  comments.forEach((comment) => {
+    scoreBoard(comment);
+  });
+};
+
+const displayReserved = async () => {
+  const scores = await retrieveFromAPI();
+  scores.forEach((score) => {
+    scoreBoard(score);
+  });
+};
+
+const comments = document.querySelector('button#comments');
+comments.addEventListener('click', displayComments);
+
+const reservations = document.querySelector('button#reservations');
+reservations.addEventListener('click', displayReserved);
