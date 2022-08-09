6eddf8a4c4851a761ccfedf26b820b508059e79d
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-08-09 14:47:42 -0300

    popUp windows

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-//import { fromPairs } from "lodash";
+import { fromPairs } from "lodash";
 import './style.css';
 
+const span = document.getElementById('popUpComments');
+const popUp1Ul = document.getElementById('commentUl');
+const popUpCommentCont = document.createElement('li');
+popUpCommentCont.classList = 'popUp1';
+popUpCommentCont.innerText = "my comment"/* `<li class="popUp1"><span>${user}</span><span class="span">${comment}</span></li>`;  */
+popUp1Ul.appendChild(popUpCommentCont);
+document.span.appendChild(popUp1Ul);
+
+// https://static.tvmaze.com/js/paq.js?v=1626190720
+/* let baseUrl = 'https://www.tvmaze.com/'
 const retrieveFromAPI = async () => {
-  const res = await fetch(`${baseUrl}/games/KiAaECHQt9kyqpbrRb7E/scores`);
-  const lead = await res.json();
-  const leaders = lead.result;
-  return leaders;
+  const com = await fetch(`${baseUrl}js/paq.js?v=1626190720/comments`);
+  const comm = await com.json();
+  const comments = comm.result;
+  return comments;
 };
-const leaderBoardWrapper = document.getElementById('scoreList');
-const scoreBoard = ({ user, score }) => {
-  leaderBoardWrapper.innerHTML += `<li class="animated bounce"><span>${user}</span><span class="span">${score}</span></li>`;
+const commentBoardWrapper = document.getElementById('commentList');
+const commentBoard = ({ user, comment }) => {
+  commentBoardWrapper.innerHTML += `<li class="animated bounce"><span>${user}</span><span class="span">${comment}</span></li>`;
 };
 
 const displayComments = async () => {
@@ -19,10 +29,24 @@ const displayComments = async () => {
   });
 };
 
+// https://static.tvmaze.com/js/paq.js?v=1626190720
+
+ const retrieveFromAPIres = async () => {
+  const res = await fetch(`${baseUrl}js/paq.js?v=1626190720/reserves`);
+  const reserv = await res.json();
+  const reserves = reserv.result;
+  return reserves;
+ };
+
+const reserveBoardWrapper = document.getElementById('reserveList');
+const reserveBoard = ({ user, reserve }) => {
+  reserveBoardWrapper.innerHTML += `<li class="animated bounce"><span>${user}</span><span class="span">${reserve}</span></li>`;
+};
+
 const displayReserved = async () => {
-  const scores = await retrieveFromAPI();
-  scores.forEach((score) => {
-    scoreBoard(score);
+  const reserves = await retrieveFromAPI();
+  reserves.forEach((reserve) => {
+    reserveBoard(reserve);
   });
 };
 
@@ -31,3 +55,4 @@ comments.addEventListener('click', displayComments);
 
 const reservations = document.querySelector('button#reservations');
 reservations.addEventListener('click', displayReserved);
+ */
