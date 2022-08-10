40936b8d8df50f9e1fdfb29aa2e6fa4b6510c579
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-08-10 18:30:39 -0300

    linter eslint css errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
-import  { fetchData } from './modules/utils.js';
-import filmtube1 from './assets/filmtube1.png';
+// import { fetchData } from './modules/utils.js';
+// import filmtube1 from './assets/filmtube1.png';
 
 fetchData();
 
@@ -8,41 +8,41 @@ const append = document.querySelector('#menu');
 export const display = (scores) => {
   for (let i = 0; i < scores?.slice(0, 6).length; i += 1) {
     // create the display page of the project.
-    const items = scores[i]
+    const items = scores[i];
     const div = document.createElement('div');
-    div?.classList.add('items')
-    const commentButton = document.createElement('button')
+    div?.classList.add('items');
+    const commentButton = document.createElement('button');
     commentButton.innerHTML = "comments";
     commentButton.classList = 'commentBtn';
-    const reservationButton = document.createElement('button')
-    reservationButton.innerHTML = "Reservations"
-    const icon = document.createElement('i')
-    icon.className = 'fa-regular fa-heart'
-    const likes = document.createElement('div')
-    likes.innerHTML = '5 likes'
-    likes.classList.add('likes')
+    const reservationButton = document.createElement('button');
+    reservationButton.innerHTML = 'Reservations';
+    const icon = document.createElement('i');
+    icon.className = 'fa-regular fa-heart';
+    const likes = document.createElement('div');
+    likes.innerHTML = '5 likes';
+    likes.classList.add('likes');
     const snacksimg = new Image(300, 300);
     snacksimg.className = 'img';
     snacksimg.setAttribute('src', items?.image);
-    const titleDiv = document.createElement('div')
-    titleDiv?.classList.add('titleDiv')
-    icon.style.lineHeight = 3.3
-    icon.style.marginLeft = '18px'
-    const title = document.createElement('p')
-    title.innerHTML = items?.title
-    titleDiv.appendChild(title)
-    titleDiv.appendChild(icon)
-    div.appendChild(snacksimg)
-    div.appendChild(titleDiv)
-    div.appendChild(likes)
-    div.appendChild(commentButton)
-    div.appendChild(reservationButton)
-    append.appendChild(div)
+    const titleDiv = document.createElement('div');
+    titleDiv?.classList.add('titleDiv');
+    icon.style.lineHeight = 3.3;
+    icon.style.marginLeft = '18px';
+    const title = document.createElement('p');
+    title.innerHTML = items?.title;
+    titleDiv.appendChild(title);
+    titleDiv.appendChild(icon);
+    div.appendChild(snacksimg);
+    div.appendChild(titleDiv);
+    div.appendChild(likes);
+    div.appendChild(commentButton);
+    div.appendChild(reservationButton);
+    append.appendChild(div);
     commentButton.addEventListener('click', () => {
-      //create the Pop up window to the comments.
+      // create the Pop up window to the comments.
       const commentPopUp = document.querySelector('.popComment');
       const div = document.createElement('div');
-      div?.classList.add('popupitems')
+      div?.classList.add('popupitems');
       const deleteBtn = document.createElement('button');
       deleteBtn.type = 'button';
       deleteBtn.classList = 'delete';
@@ -51,23 +51,23 @@ export const display = (scores) => {
       const movieImg = new Image(300, 300);
       movieImg.className = 'img';
       movieImg.setAttribute('src', items?.image);
-      const titleDiv = document.createElement('div')
-      titleDiv?.classList.add('titleDiv')
-      const title = document.createElement('h2')
-      title.innerHTML = items?.title
+      const titleDiv = document.createElement('div');
+      titleDiv?.classList.add('titleDiv');
+      const title = document.createElement('h2');
+      title.innerHTML = items?.title;
       const ul = document.createElement('ul');
       ul.classList.add('info');
-      const li1 = document.createElement('li')
+      const li1 = document.createElement('li');
       li1.classList = 'infoChild';
       li1.innerText = items?.titleOriginal;
       ul.appendChild(li1);
-      const li3 = document.createElement('li')
+      const li3 = document.createElement('li');
       li3.classList = 'infoChild';
       li3.innerHTML = items?.rating;
       ul.appendChild(li3);
-      const li4 = document.createElement('li')
+      const li4 = document.createElement('li');
       li4.classList = 'infoChild';
-      li4.innerText = items?.release;      
+      li4.innerText = items?.release;
       ul.appendChild(li4);
       title.appendChild(ul);
       div.appendChild(movieImg);
@@ -81,7 +81,7 @@ export const display = (scores) => {
       // create the Pop up window to the reservation.
       const commentPopUp = document.querySelector('.popComment');
       const div = document.createElement('div');
-      div?.classList.add('popupitems')
+      div?.classList.add('popupitems');
       const deleteBtn = document.createElement('button');
       deleteBtn.type = 'button';
       deleteBtn.classList = 'delete';
@@ -90,24 +90,24 @@ export const display = (scores) => {
       const movieImg = new Image(300, 300);
       movieImg.className = 'img';
       movieImg.setAttribute('src', items?.image);
-      const titleDiv = document.createElement('div')
-      titleDiv?.classList.add('titleDiv')
-      const title = document.createElement('h2')
+      const titleDiv = document.createElement('div');
+      titleDiv?.classList.add('titleDiv');
+      const title = document.createElement('h2');
       title.classList = 'title';
       title.innerHTML = items?.title
       const ul = document.createElement('ul');
       ul.classList.add('info');
-      const li1 = document.createElement('li')
+      const li1 = document.createElement('li');
       li1.classList = 'infoChild';
       li1.innerText = items?.titleOriginal;
       ul.appendChild(li1);
-      const li3 = document.createElement('li')
+      const li3 = document.createElement('li');
       li3.classList = 'infoChild';
       li3.innerHTML = items?.rating;
       ul.appendChild(li3);
-      const li4 = document.createElement('li')
+      const li4 = document.createElement('li');
       li4.classList = 'infoChild';
-      li4.innerText = items?.release;      
+      li4.innerText = items?.release;
       ul.appendChild(li4);
       title.appendChild(ul);
       div.appendChild(movieImg);
--- src/modules/commentPopUp.js
+++ src/modules/commentPopUp.js
@@ -1,39 +1,38 @@
-
 export const comment = (scores) => {
-    for (let i = 0; i < scores?.slice(0, 1).length; i += 1) {
-      const items = scores[i]
-      const div = document.createElement('div');
-      div?.classList.add('items')
-     const commentButton = document.createElement('button')
-     commentButton.innerHTML = "comments"
-          
-      const movieImg = new Image(300, 300);
-      movieImg.className = 'img';
-      movieImg.setAttribute('src', items?.image);
-      const titleDiv = document.createElement('div')
-      titleDiv?.classList.add('titleDiv')
-      const ul = document.createElement('ul');
-      ul.classList('info');
-      titleDiv.appendChild(ul);
-      const li1 = document.createElement('li')
-      li1.classList = 'infoChild1';
-      ul.appendChild(li1);
-      const li2 = document.createElement('li')
-      li2.classList = 'infoChild1';
-      ul.appendChild(li2);
-      const li3 = document.createElement('li')
-      li3.classList = 'infoChild3';
-      ul.appendChild(li3);
-      const li4 = document.createElement('li')
-      li4.classList = 'infoChild4';
-      ul.appendChild(li4);
-      const title = document.createElement('p') 
-      title.innerHTML = items?.title
-  
-      titleDiv.appendChild(title);
-      div.appendChild(movieImg);
-      div.appendChild(titleDiv);
-      div.appendChild(commentButton);
-      append.appendChild(div);
-    }
-    };
+  for (let i = 0; i < scores?.slice(0, 1).length; i += 1) {
+    const items = scores[i];
+    const div = document.createElement('div');
+    div?.classList.add('items');
+    const commentButton = document.createElement('button');
+    commentButton.innerHTML = "comments";
+        
+    const movieImg = new Image(300, 300);
+    movieImg.className = 'img';
+    movieImg.setAttribute('src', items?.image);
+    const titleDiv = document.createElement('div');
+    titleDiv?.classList.add('titleDiv');
+    const ul = document.createElement('ul');
+    ul.classList('info');
+    titleDiv.appendChild(ul);
+    const li1 = document.createElement('li');
+    li1.classList = 'infoChild1';
+    ul.appendChild(li1);
+    const li2 = document.createElement('li');
+    li2.classList = 'infoChild1';
+    ul.appendChild(li2);
+    const li3 = document.createElement('li');
+    li3.classList = 'infoChild3';
+    ul.appendChild(li3);
+    const li4 = document.createElement('li');
+    li4.classList = 'infoChild4';
+    ul.appendChild(li4);
+    const title = document.createElement('p');
+    title.innerHTML = items?.title;
+
+    titleDiv.appendChild(title);
+    div.appendChild(movieImg);
+    div.appendChild(titleDiv);
+    div.appendChild(commentButton);
+    append.appendChild(div);
+  }
+};
